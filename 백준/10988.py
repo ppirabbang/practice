18d2eedcae1bd3848f604ca9a461d048ed237202
hakbeom <schoolb1999@naver.com>
2025-11-28 00:37:36 +0900

    25/11/28

---
+++ 백준/10988.py
@@ -0,0 +1,8 @@
+import sys
+
+a = sys.stdin.readline().rstrip()
+
+if(a == a[::-1]):
+  print(1);
+else:
+  print(0)
---
+++ 백준/2444.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+for i in range(1, N + 1):
+    stars = 2 * i - 1
+    spaces = N - i
+    print(" " * spaces + "*" * stars)
+
+for i in range(N - 1, 0, -1):
+    stars = 2 * i - 1
+    spaces = N - i
+    print(" " * spaces + "*" * stars)
