ec73f30c3056ab3968aca693f672aacf711e78d1
hakbeom <schoolb1999@naver.com>
2026-04-13 23:56:42 +0900

    26/04/13

--- 백준/1904.py
+++ 백준/1904.py
@@ -1,10 +1,12 @@
 import sys
 
+dp = [0] * 1000001
 N = int(sys.stdin.readline())
-dp = [0] * 1000000
-dp[0] = 1
-dp[1] = 2
-for i in range(2,N):
+
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3,N+1):
   dp[i] = (dp[i-1] + dp[i-2]) % 15746
 
-print(dp[N-1])
+print(dp[N])
