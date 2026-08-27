18737d37d19ffb4f2441395b20ef4ae0268a269e
hakbeom <schoolb1999@naver.com>
2026-08-28 00:09:03 +0900

    26/08/28

---
+++ 음료수 얼려 먹기.py
@@ -0,0 +1,29 @@
+import sys
+
+n,m = map(int, sys.stdin.readline().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, sys.stdin.readline().rstrip())))
+
+def dfs(x,y):
+    if x<=-1 or x>=n or y<=-1 or y>=m:
+        return False
+
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        #상하좌우
+        dfs(x-1,y)
+        dfs(x+1,y)
+        dfs(x,y-1)
+        dfs(x,y+1)
+        return True
+    return False
+
+result = 0
+for i in range(n):
+    for j in range(n):
+        if dfs(i,j):
+            result +=1
+
+print(result)
